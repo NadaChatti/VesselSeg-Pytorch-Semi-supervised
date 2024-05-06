14688cf3b93469384f864d1d7a020926c1abde3a
NadaChatti <nadachatti95@gmail.com>
2024-05-06 15:15:47 +0200

    fixed lr decay and other issues

--- code/solver.py
+++ code/solver.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from torch import nn
 from tqdm import tqdm
 import torch.nn.functional as F
 import os
@@ -18,7 +19,7 @@ class Solver(object):
 
     """
 
-    def __init__(self, model, train_dataloader, val_dataloader, device,
+    def __init__(self, model:nn.Module, train_dataloader, val_dataloader, device,
                  optimizer: torch.optim.Optimizer, learning_rate=1e-3, beta=0.3, 
                  labeled_bs=4, scaling=1500, consistency_rampup=40.0,
                  max_iterations=6000,
@@ -47,7 +48,7 @@ class Solver(object):
         self.model = model
         self.device = device
 
-        self.learning_rate = learning_rate
+        self.base_learning_rate = learning_rate
         self.beta = beta
         self.scaling = scaling
         self.labeled_bs = labeled_bs
@@ -67,8 +68,8 @@ class Solver(object):
         self.train_dataloader = train_dataloader
         self.val_dataloader = val_dataloader
         
-        self.current_patience = 0
-
+        self.current_iter = 0
+        self.current_lr = self.base_learning_rate
         self._reset()
 
     def _reset(self):
@@ -87,26 +88,24 @@ class Solver(object):
         self.val_batch_loss = []
 
         self.num_operation = 0
-        self.current_patience = 0
+        self.current_iter = 0
 
-    def _step(self, bat_img, bat_label, iter_num, validation=False):
+
+    def _forward(self, batch, validation):
+        """
+        Forward pass through the model. Updates metric object with current stats.
+
+        Args:
+            batch (dict): Input data batch.
+            metrics: Metrics object for tracking.
+        """
         def get_current_consistency_weight(epoch):
             # Consistency ramp-up from https://arxiv.org/abs/1610.02242
             return sigmoid_rampup(epoch, self.consistency_rampup)
-        """
-        Make a single gradient update. This is called by train() and should not
-        be called manually.
-
-        :param bat_img: batch of training features
-        :param bat_label: batch of corresponding training labels
-        :param validation: Boolean indicating whether this is a training or
-            validation step
-
-        :return loss: Loss between the model prediction for X and the target
-            labels y
-        """
         loss = None
-
+        # Unpack data
+        bat_img, bat_label = batch['image'], batch['label']
+        bat_img, bat_label = bat_img.to(self.device), bat_label.to(self.device)
         # Forward pass
         bat_pred_tanh, bat_pred = self.model(bat_img)
         bat_pred_soft = torch.sigmoid(bat_pred)
@@ -123,104 +122,112 @@ class Solver(object):
         consistency_loss = torch.mean((dis_to_mask - bat_pred_soft) ** 2)
         supervised_loss = loss_seg_dice + self.beta * loss_sdf
         
-        consistency_weight = get_current_consistency_weight((iter_num - 1) // 150)
+        consistency_weight = get_current_consistency_weight((self.current_iter) // 150)
         loss = supervised_loss + consistency_weight * consistency_loss
-        # Add the regularization
-        # loss += sum(self.model.reg.values())
-
-        # Count number of operations
-        # self.num_operation += self.model.num_operation
-
-        # Perform gradient update (only in train mode)
-        if not validation:
-            self.opt.zero_grad()
-            # Compute gradients
-            loss.backward()
-            # Update weights
-            self.opt.step()
-            # If it was a training step, we need to count operations for
-            # backpropagation as well
-            # self.num_operation += self.model.num_operation
 
         # write to TensorBoard
         segment = 'train' if not validation else 'val'
-        self.writer.add_scalar(f'{segment}/loss', loss, iter_num)
-        self.writer.add_scalar(f'{segment}/loss_dice', loss_seg_dice, iter_num)
-        self.writer.add_scalar(f'{segment}/loss_hausdorff', loss_sdf, iter_num)
-        self.writer.add_scalar(f'{segment}/consistency_weight', consistency_weight, iter_num)
-        self.writer.add_scalar(f'{segment}/consistency_loss', consistency_loss, iter_num)
+        self.writer.add_scalar('lr', self.current_lr, self.current_iter)
+        self.writer.add_scalar(f'consistency_weight', consistency_weight, self.current_iter)
+        self.writer.add_scalar(f'{segment}_loss/loss', loss.item(), self.current_iter)
+        self.writer.add_scalar(f'{segment}_loss/loss_dice', loss_seg_dice.item(), self.current_iter)
+        self.writer.add_scalar(f'{segment}_loss/loss_hausdorff', loss_sdf.item(), self.current_iter)
+        self.writer.add_scalar(f'{segment}_loss/consistency_loss', consistency_loss.item(), self.current_iter)
+        # if self.current_iter % self.save_every == 0:
         self.save_tb_images_training(bat_img[0, :, :, :],
                     bat_pred_soft[0, :, :, :],
                     bat_label[0, :, :],
                     dis_to_mask[0, 0:1, :, :],
                     bat_pred_tanh[0, 0:1, :, :],
                     gt_dis[0, :, :], 
-                    iter_num, validation)
+                    self.current_iter, segment)
         logging.info('iteration %d : loss : %f, loss_consis: %f, loss_haus: %f, loss_seg: %f, loss_dice: %f' %
-            (iter_num, loss.item(), consistency_loss.item(), loss_sdf.item(),
+            (self.current_iter, loss.item(), consistency_loss.item(), loss_sdf.item(),
             loss_seg.item(), loss_seg_dice.item()))
+        
+        # change lr
+        if self.current_iter % 1000 == 0:
+            self.current_lr = self.base_learning_rate * 0.1 ** (self.current_iter // 2500)
+            for param_group in self.opt.param_groups:
+                param_group['lr'] = self.current_lr
+        if self.current_iter % self.save_every == 0:
+            save_model_path = os.path.join(self.model_path, 'iter_' + str(self.current_iter) + '.pth')
+            torch.save(self.model.state_dict(), save_model_path)
+            logging.info("save model to {}".format(save_model_path))
+
         return loss
 
+    def _train_loop(self):
+        """
+        Executes the training loop.
+
+        Handles the iteration over training data batches and performs backpropagation.
+        """
+        self.model.train()
+        self.opt.zero_grad()
+        self.empty_cache()
+        train_epoch_loss = 0
+        # Iterate over all training samples
+        for batch in self.train_dataloader:
+                self.current_iter += 1
+                # Update the model parameters.
+                loss = self._forward(batch, validation=False)
+                # Compute gradients
+                loss.backward()
+                # Update weights
+                self.opt.step()
+                
+                self.train_batch_loss.append(loss)
+                train_epoch_loss += loss
+
+                if self.current_iter >= self.max_iterations:
+                        break    
+                
+        self.empty_cache()
+        return train_epoch_loss
+
+    def _eval_loop(self):
+        """
+        Executes the evaluation loop.
+
+        Handles the iteration over validation data batches for evaluation purposes.
+        """
+        self.model.eval()
+        self.empty_cache()
+        val_epoch_loss = 0
+
+        with torch.no_grad():
+            # Iterate over all validation samples
+            for batch in self.val_dataloader:
+                self.current_iter += 1
+
+                # Update the model parameters.
+                loss = self._forward(batch, validation=True)
+                
+                self.val_batch_loss.append(loss)
+                val_epoch_loss += loss
+        
+        self.empty_cache()
+        return val_epoch_loss
+                        
     # def train(self, patience = None):
     def train(self):
         """
         Run optimization to train the model.
         """
-        iter_num = 0
         max_epoch = self.max_iterations//(len(self.train_dataloader) + len(self.val_dataloader))+1
         iterator = tqdm(range(max_epoch), ncols=70)
+        
         # Start an epoch
         for epoch in iterator:
             logging.info("\n[+] ====== Start training... epoch {} ======".format(epoch + 1))
             # print(torch.cuda.memory_summary(device=None, abbreviated=False))
-            # Iterate over all training samples
-            train_epoch_loss = 0.0
-
-            for batch in self.train_dataloader:
-                iter_num += 1
-                self.model.train()
-                # Unpack data
-                bat_img, bat_label = batch['image'], batch['label']
-                bat_img, bat_label = bat_img.to(self.device), bat_label.to(self.device)
-
-                # Update the model parameters.
-                validate = False
-                train_loss = self._step(bat_img, bat_label, iter_num, validation=validate)
-
-                self.train_batch_loss.append(train_loss)
-                train_epoch_loss += train_loss
-
-                # change lr
-                if iter_num % 150 == 0:
-                    lr_ = self.learning_rate * 0.1 ** (iter_num // 2500)
-                    for param_group in self.opt.param_groups:
-                        param_group['lr'] = lr_
-                if iter_num % self.save_every == 0:
-                    save_model_path = os.path.join(self.model_path, 'iter_' + str(iter_num) + '.pth')
-                    torch.save(self.model.state_dict(), save_model_path)
-                    logging.info("save model to {}".format(save_model_path))
-                if iter_num >= self.max_iterations:
-                        break    
-                
             
+            train_epoch_loss = self._train_loop()
             train_epoch_loss /= len(self.train_dataloader)    
-            
-            # Iterate over all validation samples
-            val_epoch_loss = 0.0
 
-            for batch in self.val_dataloader:
-                iter_num += 1
-                self.model.eval()
-                # Unpack data
-                bat_img, bat_label = batch['image'], batch['label']
-                bat_img, bat_label = bat_img.to(self.device), bat_label.to(self.device)
-
-                # Compute Loss - no param update at validation time!
-                val_loss = self._step(bat_img, bat_label, iter_num, validation=True)
-                self.val_batch_loss.append(val_loss)
-                val_epoch_loss += val_loss
-
-            val_epoch_loss /= len(self.val_dataloader)
+            val_epoch_loss = self._eval_loop()
+            val_epoch_loss /= len(self.val_dataloader)  
 
             # Record the losses for later inspection.
             self.train_loss_history.append(train_epoch_loss)
@@ -231,11 +238,11 @@ class Solver(object):
                     epoch + 1, len(iterator), train_epoch_loss.item(), val_epoch_loss.item()))
 
             # Keep track of the best model
-            self.update_best_loss(val_epoch_loss, train_epoch_loss, iter_num)
+            self.update_best_loss(val_epoch_loss, train_epoch_loss, self.current_iter)
             # if patience and self.current_patience >= patience:
             #     logging.info("Stopping early at epoch {}!".format(epoch))
             #     break
-            if iter_num >= self.max_iterations:
+            if self.current_iter >= self.max_iterations:
                 iterator.close()
                 break
         # At the end of training swap the best params into the model
@@ -269,24 +276,31 @@ class Solver(object):
 
     def save_tb_images_training(self,img, prediction, label, 
                     dis_to_mask, pred_dist_map, gt_distmap, 
-                    iteration, validation):
-        segment = 'train' if not validation else 'val'
+                    iteration, segment):
         grid_image = make_grid(img, 1, normalize=True)
-        self.writer.add_image(f'{segment}/Image', grid_image, iteration)
+        self.writer.add_image(f'{segment}_vis/Image', grid_image, iteration)
 
         grid_image = make_grid(prediction, 1, normalize=False)
-        self.writer.add_image(f'{segment}/Predicted_label', grid_image, iteration)
+        self.writer.add_image(f'{segment}_vis/Predicted_label', grid_image, iteration)
 
         grid_image = make_grid(label, 1, normalize=False)
-        self.writer.add_image(f'{segment}/Groundtruth_label',
+        self.writer.add_image(f'{segment}_vis/Groundtruth_label',
                             grid_image, iteration)
 
         grid_image = make_grid(dis_to_mask, 1, normalize=False)
-        self.writer.add_image(f'{segment}/Dis2Mask', grid_image, iteration)
+        self.writer.add_image(f'{segment}_vis/Dis2Mask', grid_image, iteration)
 
         grid_image = make_grid(pred_dist_map, 1, normalize=False)
-        self.writer.add_image(f'{segment}/DistMap', grid_image, iteration)
+        self.writer.add_image(f'{segment}_vis/DistMap', grid_image, iteration)
             
         grid_image = make_grid(gt_distmap, 1, normalize=False)
-        self.writer.add_image(f'{segment}/Groundtruth_DistMap',
+        self.writer.add_image(f'{segment}_vis/Groundtruth_DistMap',
                             grid_image, iteration)
+
+    def empty_cache(self):
+        """
+        Empties the GPU cache if it is used.
+        """
+
+        if self.device == torch.device("cuda:0"):
+            torch.cuda.empty_cache()
